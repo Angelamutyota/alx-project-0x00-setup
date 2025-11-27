6e3ead8d524ff3e291cc0c5a09e2c21d27b602e7
angelamutyotajiji <angela.mutyota@jiji.health>
2025-11-27 21:22:59 +0300

    add landing page content

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,23 @@
-const Landing: React.FC =  () => {
+import React from "react";
+import Card from "@/components/Card";
+
+const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
+    <div className="p-6">
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+
+      {/* Render one Card */}
+      <div className="mt-6">
+        <Card />
+      </div>
+
+      {/* Duplicate to see multiple cards */}
+      <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <Card />
+        <Card />
+      </div>
     </div>
-  )
-}
-export default Landing
+  );
+};
+
+export default Landing;
